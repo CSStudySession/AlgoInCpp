0fa1e77af5bca345e35801260d94c9fb16d0a4f6
aarliu <aarliu@cisco.com>
2019-11-28 23:26:23 -0800

    created new files for shuati

---
+++ TwoPointers/LC1TwoSum.cpp
@@ -0,0 +1,20 @@
+//
+// Created by aarliu on 11/28/19.
+//
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class LC1TwoSum {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> result;
+        return result;
+    }
+};
+
+int main() {
+    std::cout << "Hello, World in two sum!" << std::endl;
+    return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    std::cout << "Hello, World in top folder!" << std::endl;
+    return 0;
+}
